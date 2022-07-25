45a3fc8ff7098d9cd99e88ec4c4e8f6a485f247e
martsDev <martssilvapro@gmail.com>
2022-07-25 15:45:19 -0300

    adiciona mais complexidade a classe

--- poo-01/firstClass.js
+++ poo-01/firstClass.js
@@ -36,3 +36,35 @@ console.log(person1.greeting());
 
 console.log(person2.name);
 console.log(person2.greeting());
+
+//=================================
+
+// Um pouco mais de complexidade
+
+class Person {
+    constructor(firstName, lastName, age, gender, height, interests) {
+
+        this.name = {
+            'firstName': firstName,
+            'lastName': lastName
+        };
+
+        this.age = age;
+
+        this.gender = gender;
+
+        this.height = height;
+
+        this.interests = interests;
+
+        this.bio = function() {
+            return `
+            Hello World, my name is ${this.name.firstName} ${this.name.lastName}, I'm ${this.age}! My heigth is ${this.height}
+            and I like things such as ${this.interests}`
+        };
+    };
+};
+
+let hero = new Person('Lucas', 'Silva', 21, 'male', '1.70', ['MMORPG', 'RPG', 'Cardgames', 'guitar', 'books', 'tec']);
+
+console.log(hero.bio());
