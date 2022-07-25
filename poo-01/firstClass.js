87a29bcba2544a95ec712ce0d7cd2b35c02575bc
martsDev <martssilvapro@gmail.com>
2022-07-25 15:23:36 -0300

    implementa primeira classe construtora

--- poo-01/firstClass.js
+++ poo-01/firstClass.js
@@ -1,11 +1,30 @@
+// Modo 'prolixo'
+
 function createNewPerson(name) {
     var obj = {};
 
     obj.name = name;
     obj.greeting = function() {
-        console.log('Eae, me chamo ' + obj.name + '!')
+        return 'Eae, me chamo ' + obj.name + '!'
     };
 
     return obj;
 };
 
+let lucas = createNewPerson('Lucas');
+console.log(lucas.name);
+console.log(lucas.greeting());
+
+// ============================
+
+// Modo otimizado - Função Construtora
+
+function Person(name) {
+    obj.name = name;
+    obj.greeting = function() {
+        return 'Eae, me chamo ' + obj.name + '!'
+    };
+};
+
+let person1 = new Person('Lucas');
+let person2 = new Person('Zé');
