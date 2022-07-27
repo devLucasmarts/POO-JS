e17a2570389567f4e9bba56e8313eb171798c0e2
martsDev <martssilvapro@gmail.com>
2022-07-27 10:13:18 -0300

    implementa métodos privados e públicos

--- poo-01/oop-pillars.js
+++ poo-01/oop-pillars.js
@@ -14,5 +14,14 @@ class Person {
         return this._age;
     };
 
-    
-}
+    setAge(newAge) {
+        if (newAge >= 0 && newAge < 200) {
+            this._age = newAge;
+        };
+    };
+};
+
+const p1 = new Person('Zé', 170, 70, 22);
+p1.setAge(23);
+
+console.log(p1.name, p1.height, p1.getWeight(), p1.getAge());
