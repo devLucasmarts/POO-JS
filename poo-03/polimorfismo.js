9aa37100aa5de05666359942008f0ada0758dba8
martsDev <martssilvapro@gmail.com>
2022-07-28 15:42:42 -0300

    Implementa mais classes com poliformismo

--- poo-03/polimorfismo.js
+++ poo-03/polimorfismo.js
@@ -4,19 +4,30 @@ class Human {
         this.weapon = weapon;
     };
 
-    getWeapon() { return `O ${this.characterClass} equipou o(a) ${this.weapon}`};
+    getWeapon() {};
 };
 
 class Jedi extends Human {
 
     getWeapon() { return `O ${this.characterClass} golpeou com o ${this.weapon}` };
+};
 
+class Sith extends Human {
+
+    getWeapon() { return `O ${this.characterClass} arremessou o ${this.weapon}` };
 };
 
 const hero = new Jedi('Jedi', 'lightsaber');
+
+const villain = new Sith('Darth Sith', 'lightsaber');
 
 const useHero = (hero) => {
     return hero.getWeapon()
 };
 
+const useVillain = (villain) => {
+    return villain.getWeapon()
+};
+
 console.log(useHero(hero));
+console.log(useVillain(villain));
