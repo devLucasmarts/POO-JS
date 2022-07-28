3d77b2ae1eb403a248cbfcbe7b32748633adf984
martsDev <martssilvapro@gmail.com>
2022-07-28 16:50:44 -0300

    implementa interface Person e classe PhysicalPerson

---
+++ poo-03/interfacesEgenerics.ts
@@ -0,0 +1,33 @@
+interface Person {
+    id: number;
+    name: string;
+    showIdentification(): void;
+};
+
+class PhysicalPerson implements Person {
+    private static lastId = 0;
+    private _name;
+    private _id;
+    private _cpf;
+
+    constructor(name: string, cpf: string) {
+        this._id = PhysicalPerson.newId();
+        this._name = name;
+        this._cpf = cpf;
+    };
+
+    private static newId() {
+        return this.lastId++;
+    };
+
+    get id() { return this._id }
+
+    get name() { return this._name }
+
+    get cpf() { return this._cpf }
+
+    showIdentification(): void {
+        console.log(this.id, this._cpf);
+    };
+};
+
