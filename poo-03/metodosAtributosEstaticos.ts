6274e20073992dd1f29867af94bd79d3a1279a8c
martsDev <martssilvapro@gmail.com>
2022-07-28 16:17:19 -0300

    Implementa atributos e metodos estaticos

---
+++ poo-03/metodosAtributosEstaticos.ts
@@ -0,0 +1,16 @@
+class Employee {
+    private static employeeCount = 0;
+
+    constructor(public name: string) {
+        Employee.addEmployee();
+    };
+
+    private static addEmployee() {
+        this.employeeCount += 1;
+    };
+
+    static get employees() {
+        return this.employeeCount;
+    };
+};
+
